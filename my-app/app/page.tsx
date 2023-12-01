3158db7734e01584c87e80a8a21bc2260351efb6
Arghun <arghava.mousanezhad@utopiamusic.com>
2023-12-01 09:12:12 +0100

    implement tRPC to render on the server in app router

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,9 +1,12 @@
+import { serverClient } from "@/app/_trpc/serverClient";
+
 import TodoList from "@/components/TodoList";
 
-export default function Home() {
+export default async function Home() {
+  const todos = await serverClient.getTodos();
   return (
     <div className="container m-16">
-      <TodoList />
+      <TodoList initialTodos={todos} />
     </div>
   );
 }
--- my-app/components/TodoList.tsx
+++ my-app/components/TodoList.tsx
@@ -2,9 +2,18 @@
 
 import { useState } from "react";
 import { trpc } from "@/app/_trpc/client";
+import { serverClient } from "@/app/_trpc/serverClient";
 
-export default function TodoList() {
-  const getTodos = trpc.getTodos.useQuery();
+export default function TodoList({
+  initialTodos,
+}: {
+  initialTodos: Awaited<ReturnType<(typeof serverClient)["getTodos"]>>;
+}) {
+  const getTodos = trpc.getTodos.useQuery(undefined, {
+    initialData: initialTodos,
+    refetchOnMount: false,
+    refetchOnReconnect: false,
+  });
   const addTodo = trpc.addTodo.useMutation({
     onSettled: () => {
       getTodos.refetch();
